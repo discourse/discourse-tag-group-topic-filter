1921d62476e9aacb102ffb2d48f8cba9e8a6ffa7
Juan David Martinez <juan@discourse.org>
2023-09-04 16:30:26 -0500

    completed 'core styling' fixed bugs

--- javascripts/discourse/components/custom-filter.js
+++ javascripts/discourse/components/custom-filter.js
@@ -1,13 +1,16 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
+import { TrackedArray } from "@ember-compat/tracked-built-ins";
+import { tracked } from "@glimmer/tracking";
+import DiscourseURL from "discourse/lib/url";
 
 export default class CustomFilter extends Component {
   @service siteSettings;
   @service router;
 
-  selectedTags = [];
-  category = 0;
+  @tracked selectedTags = new TrackedArray();
+  category = {};
 
   constructor() {
     super(...arguments);
@@ -16,28 +19,36 @@ export default class CustomFilter extends Component {
 
   _initializeSelectedTags() {
     const currentRoute = this.router.currentRoute;
-    const { params } = currentRoute;
-    if (params.intersection) {
-      const selectedTags = params.intersection.split("/");
-      this.selectedTags = selectedTags;
+    if (currentRoute.name == "tags.intersection") {
+      this.selectedTags = [
+        currentRoute.attributes?.tag.id,
+        ...currentRoute.attributes?.additionalTags,
+      ];
     }
-    this.category = currentRoute.attributes?.category;
+    this.category = currentRoute.attributes?.category
+      ? currentRoute.attributes?.category
+      : { id: currentRoute.queryParams.category };
   }
 
   get tagGroups() {
-    debugger;
     return [
       {
         name: settings.first_tag_group_name,
         tags: settings.first_group_tag.split("|"),
+        primaryColor: settings.first_group_tag_primary_color,
+        secondaryColor: settings.first_group_tag_secondary_color,
       },
       {
         name: settings.second_tag_group_name,
         tags: settings.second_group_tag.split("|"),
+        primaryColor: settings.second_group_tag_primary_color,
+        secondaryColor: settings.second_group_tag_secondary_color,
       },
       {
         name: settings.third_tag_group_name,
         tags: settings.third_group_tag.split("|"),
+        primaryColor: settings.third_group_tag_primary_color,
+        secondaryColor: settings.third_group_tag_secondary_color,
       },
     ];
   }
@@ -62,7 +73,6 @@ export default class CustomFilter extends Component {
     );
   }
 
- 
   @action
   selectTag(tag) {
     this.selectedTags.push(tag);
@@ -75,7 +85,12 @@ export default class CustomFilter extends Component {
 
   @action
   applyFilters() {
-    const tagsPath = this.selectedTags.join('/');
-    this.router.transitionTo({ queryParams: { intersection: tagsPath } });
+    console.log("transition");
+    const tagsPath = this.selectedTags.join("/");
+    const category = this.category;
+    const transitionURL = category
+      ? `/tags/intersection/${tagsPath}?category=${category.id}`
+      : `/tags/intersection/${tagsPath}`;
+    DiscourseURL.routeTo(transitionURL, { queryParams: { category: 4 } });
   }
 }
--- javascripts/discourse/components/filter-tag.js
+++ javascripts/discourse/components/filter-tag.js
@@ -1,18 +1,50 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
-
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
+import { capitalize } from "@ember/string";
+import { htmlSafe } from "@ember/template";
 export default class StatusTagsComponent extends Component {
   @tracked isChecked = this.args.selectedTags.includes(this.tagName);
+  filterPrimaryColor =
+    this.args.primaryColor ||
+    getComputedStyle(document.documentElement).getPropertyValue(
+      "--primary-medium"
+    );
+  filterSecondaryColor =
+    this.args.secondaryColor ||
+    getComputedStyle(document.documentElement).getPropertyValue(
+      "--primary-low"
+    );
 
+  get style() {
+    if (this.filterPrimaryColor && this.filterSecondaryColor) {
+      console.log("group style");
+      const backgroundColor = this.isChecked
+        ? this.filterPrimaryColor
+        : this.filterSecondaryColor;
+
+      const hoverColor = !this.isChecked
+        ? this.filterPrimaryColor
+        : this.filterSecondaryColor;
+
+      return htmlSafe(
+        `--hover-color: ${hoverColor}; --background-color: ${backgroundColor};`
+      );
+    }
+
+    return "";
+  }
+
+  get idTagName() {
+    return this.args.name + "-custom-tag";
+  }
   get tagName() {
-    return this.args.name;
+    return capitalize(this.args.name);
   }
 
   @action
   toggleTag() {
     this.isChecked = !this.isChecked;
-
     if (this.isChecked) {
       this.args.selectTag(this.tagName);
     } else {
