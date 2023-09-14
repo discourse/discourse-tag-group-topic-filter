2b90c3df110c1e0e9c4fdc0f2bb4094765e1aa54
Juan David Martinez <juan@discourse.org>
2023-09-14 02:56:54 -0500

    changes to filter-tag, added test for filter-tag and removed unwanted js file

--- javascripts/discourse/components/filter-tag.js
+++ javascripts/discourse/components/filter-tag.js
@@ -1,7 +1,6 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
-import { capitalize } from "@ember/string";
 import { htmlSafe } from "@ember/template";
 export default class StatusTagsComponent extends Component {
   @tracked isChecked = this.args.selectedTags.includes(this.tagName);
@@ -39,7 +38,7 @@ export default class StatusTagsComponent extends Component {
     return this.args.name + "-custom-tag";
   }
   get tagName() {
-    return capitalize(this.args.name);
+    return this.args.name;
   }
 
   @action
